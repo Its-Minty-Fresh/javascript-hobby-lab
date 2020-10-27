cdd5c9e3e275ee8611de4c39d70c0fdbf3bb88a2
Matthew Kapitan <matthewkapitan@gmail.com>
2020-10-27 14:40:03 -0500

    Added BAC Calculator

--- code.js
+++ code.js
@@ -13,6 +13,29 @@ function onClick() {
     numClicks++;
 }
 
+function calcBac() {
+    var beer = parseInt(document.getElementById("beer").value) * 0.54;
+    var wine = parseInt(document.getElementById("wine").value) * 0.6;
+    var shot = parseInt(document.getElementById("shot").value) * 0.6;
+    var weight = parseInt(document.getElementById("weight").value);
+    var hours = parseInt(document.getElementById("hours").value);
+
+
+    var x = (beer + wine + shot) * 7.5;
+    var y = x / weight;
+    var bac = y - (hours * .015);
+
+    bac = +bac.toFixed(3);
+
+    if(bac < 0){
+        bac = 0;
+    }
+
+    document.getElementById("bac").innerHTML = 'Your Blood Alcohol Content is: '+ bac + '%';
+}
+
+
+
 function showClicks() {
     if (numClicks === 0){
         alert("You have not clicked the button yet!");
